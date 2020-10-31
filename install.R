bcf40dc8b13b6915e07258d76cde3ff6a3d78c17
Dev Paudel <dev.paudel@ufl.edu>
2020-10-31 18:03:54 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,3 @@
-install.packages(c("dplyr","ggplot2","gapit"))
+install.packages(c("dplyr","ggplot2","gplots","multtest","LDheatmap","genetics","MASS","compiler","RColorBrewer"))
+source("http://www.zzlab.net/GAPIT/emma.txt")
+source("http://www.zzlab.net/GAPIT/gapit_functions.txt")
